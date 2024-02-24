21088ba176814662d2e73344ba7aef8e5f45ce34
Fernando Lecca <fernandolecca93@gmail.com>
2024-02-24 18:03:42 -0500

    refactor: Setting up basic router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
 import "./styles/main.scss";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/plans",
+    element: <div>Plans</div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
